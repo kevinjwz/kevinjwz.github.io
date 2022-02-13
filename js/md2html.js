7d147dcb856dd3ee6893fdbd262bafed4af57234
kevinjwz <kevinjwz@live.com>
2022-02-13 19:53:27 +0800

    markdown-it-attrs; table cell markers

--- js/md2html.js
+++ js/md2html.js
@@ -1,8 +1,11 @@
 import furigana from "./markdownitFurigana.js";
-window.mdit = markdownit({ html: true })
-    .use(window.markdownitMultimdTable, { rowspan: true, headerless: true, })
+window.mdit = markdownit({ html: true, breaks: true })
+    .use(furigana)
+    .use(window.markdownitMultimdTable, { rowspan: true, headerless: true })
     .use(window.markdownitMark)
-    .use(furigana);
+    .use(window.markdownItAttrs);
+
+
 
 import normalize from "./path-normalize.js";
 
@@ -16,29 +19,78 @@ mdpath = mdpath === "" ? "/index" : mdpath;
 let mdxhr = new XMLHttpRequest();
 mdxhr.addEventListener("load", onMdLoaded);
 mdxhr.open("GET", mdpath + ".md");
+if (pageAccessedByReload()) {
+    mdxhr.setRequestHeader("Cache-Control", "no-cache, no-store, max-age=0");
+}
 mdxhr.send();
 
+/** @this {XMLHttpRequest} */
 function onMdLoaded() {
+
     let mdcontent = document.getElementById("mdcontent");
 
     let md = this.responseText;
-    mdcontent.innerHTML = mdit.render(convertSpan(md));
-    convertHref(mdcontent);
-
+    mdcontent.innerHTML = mdit.render(convertLangBlock(md));
     let h1 = mdcontent.getElementsByTagName("H1");
     if (h1.length > 0) {
         document.title = h1[0].innerText;
     }
+    
+    convertHref(mdcontent);
+    convertTableMarkers(mdcontent);
+
 
     if (location.hash !== "") {
         location.replace(location.href);
     }
 }
 
-function convertSpan(md) {
-    let pattern = /(?<!\\)'([^']+)(?<!\\)'/g;
-    let span = '<span lang="ja">$1</span>';
-    return md.replace(pattern, span);
+// function convertSpan(md) {
+//     let pattern = /(?<!\\)'([^']+)(?<!\\)'/g;
+//     let span = '<span lang="ja">$1</span>';
+//     return md.replace(pattern, span);
+// }
+
+/**@param {String} md*/
+function convertLangBlock(md) {
+    // remove \ + \n
+    md = md.replaceAll(/(?<!\\)\\\r?\n/g, "");
+
+    let delim = /(^'''$|^\[\[\[$|^\]\]\]$|^\.$)/m;
+    let single_quoted_pattern = /(?<!\\)'([^']+)(?<!\\)'/g;
+    let segments = md.split(delim);
+    let outputs = [];
+    let inside_triple_quote = false;
+    for (let seg of segments) {
+        if (seg === "'''") {
+            if (!inside_triple_quote) {
+                outputs.push('<div lang="ja">\n');
+            }
+            else {
+                outputs.push('</div>\n');
+            }
+            inside_triple_quote = !inside_triple_quote;
+        }
+        else if (seg === "[[[") {
+            outputs.push('<div class="boxed">\n')
+        }
+        else if (seg === "]]]") {
+            outputs.push('</div>\n')
+        }
+        else if (seg === ".") {
+            outputs.push('<p></p>\n')
+        }
+        else {
+            if (inside_triple_quote) {              
+                outputs.push(seg.replaceAll(single_quoted_pattern, '<span lang>$1</span>'));               
+            }
+            else {
+                outputs.push(seg.replaceAll(single_quoted_pattern, '<span lang="ja">$1</span>'));
+            }
+        }
+        
+    }
+    return outputs.join("");
 }
 
 
@@ -47,9 +99,8 @@ let http_pattern = /^https?:\/\//;
 
 /** @param {HTMLElement} elem  */
 function convertHref(elem) {
-    if (elem.tagName === "A") {
-        /** @type {HTMLAnchorElement}  */
-        let a = elem;
+    let links = elem.getElementsByTagName("a");
+    for (let a of links) {
         let href = a.getAttribute("href");
         if (href.startsWith("#") || http_pattern.test(href)) {
             return;
@@ -70,9 +121,43 @@ function convertHref(elem) {
         a.pathname = htmlpath;
         a.search = "?" + mdpath;
     }
-    else {
-        for (let child of elem.children) {
-            convertHref(child);
+}
+
+/** @param {HTMLElement} elem  */
+function convertTableMarkers(elem) {
+    let tables = elem.getElementsByTagName("table");
+    for (let table of tables) {
+        let markersStr = table.getAttribute("markers");
+        if (markersStr === null)
+            continue;
+
+        let markers = new Map();
+        for (let kvStr of markersStr.split(/;\s?|,\s?|\s+/)) {
+            let kv = kvStr.match(/(?<key>[^\s])=(?<value>[^\s]+)/);
+            markers.set(kv.groups.key, kv.groups.value);
+        }
+
+        for (let cell of table.querySelectorAll("th, td")) {
+            let last = cell.lastChild;
+            if (last === null || last.nodeType !== Node.TEXT_NODE)
+                continue;
+            let str = last.textContent.trimEnd();
+            let i = str.length - 1;
+            for (; i >= 0; --i){
+                let cls = markers.get(str[i]);
+                if (cls === undefined)
+                    break;
+                cell.classList.add(cls);
+            }
+            last.textContent = str.substring(0, i + 1).trimEnd();
         }
     }
+}
+
+function pageAccessedByReload() {
+    return (window.performance.navigation && window.performance.navigation.type === 1)
+        || window.performance
+            .getEntriesByType('navigation')
+            .map((nav) => nav.type)
+            .includes('reload');
 }
